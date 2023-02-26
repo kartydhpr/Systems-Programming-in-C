9271bd1347625e9407f389d1c154e57164316696
Karteikay Dhuper <karteikaydhuper@Kartys-Laptop.local>
2023-02-25 22:51:33 -0500

    refactor code, implement string reversal and palindrome check

--- Lab 3/lab3.c
+++ Lab 3/lab3.c
@@ -37,12 +37,13 @@
  *                  Bubble Sort when there is more completely unsorted elements.
  *  Quick Sort: Definitely the quickest sorting technique with the least amount of
  *              moves for larger arrays with more unsorted elements. The divide
- *              and conquer nature of this algorithm is what makes have more moves
- *              even when the inputted array is completely or almost sorted.
+ *              and conquer nature of this algorithm is what makes it have more moves
+ *              initially even when the inputted array is completely or almost sorted.
  *
  ===================================================================================*/
 
 #include <stdio.h>
+#include <string.h>
 //global variables
    int bubble_moves=0;
    int selection_moves=0;
@@ -53,10 +54,10 @@ int menu()
 {
     int menu_decision = 3;
     while(menu_decision > 2 || menu_decision < 1) { // makes sure program doesn't crash
-        printf("[!] ------------------------- Menu ----------------------- [!] \n");
-        printf(" | • Option 1: Run sorting algorithms on an array \n");
-        printf(" | • Option 2: Use recursion to reverse a string \n");
-        printf("[!] ------------------------------------------------------ [!] \n");
+        printf("[!] ------------------------------------ Menu ---------------------------------- [!] \n");
+        printf(" | • Option 1: Run sorting algorithms on an array and compare number of moves \n");
+        printf(" | • Option 2: Use recursion to reverse a string and check if it is a palindrome \n");
+        printf("[!] ---------------------------------------------------------------------------- [!] \n");
         printf("Please return the number of the option you want to run (1 or 2): ");
         scanf("%d", &menu_decision);
     }
@@ -72,7 +73,7 @@ int menu()
     return(menu_decision);
 }
 
-//swap function for sorting algorithms
+// swap function for sorting algorithms
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
@@ -98,7 +99,7 @@ void bubbleSort(int old_arr[], int size)
 // Selection Sort
 void selectionSort(int old_arr[], int size) {
     int i, j, min_idx;
-    // One by one move boundary of unsorted subarray
+    // Move the boundary of the unsorted subarray one-by-one
     for (i = 0; i < size-1; i++) {
         // Find the minimum element in unsorted array
         min_idx = i;
@@ -112,7 +113,7 @@ void selectionSort(int old_arr[], int size) {
     }
 }
 
-// recursive quick sort function
+// Quick Sort
 void quickSort(int arr[], int left, int right) {
     int i = left, j = right; // Initialize two pointers, i and j, to the left and right ends of the current subarray
     int pivot = arr[(left + right) / 2]; // Choose a pivot element, here we choose the middle element
@@ -123,7 +124,7 @@ void quickSort(int arr[], int left, int right) {
         while (arr[j] < pivot) { // Move the right pointer to the left until we find an element that is greater than or equal to the pivot
             j--;
         }
-        if (i <= j) { // If the pointers haven't crossed each other and we've found two elements to swap
+        if (i <= j) { // If the pointers haven't crossed each other, and we've found two elements to swap
             swap(&arr[i], &arr[j]); // Swap the two elements
             i++; // Move the left pointer to the right
             j--; // Move the right pointer to the left
@@ -138,6 +139,56 @@ void quickSort(int arr[], int left, int right) {
     }
 }
 
+// Print Array function for main
+void printArr(int arr[], int arr_len){
+    for(int i = 0; i < arr_len; i++)
+    {
+        if(i == arr_len-1)
+        {
+            printf("%d\n", arr[i]);
+        }
+        else{
+            printf("%d, ", arr[i]);
+        }
+    }
+}
+
+// Pass-by-Value String Reversal Implementation
+void passByValueReversal(char str[], int start, int end) {
+    if (start>=end) // this is the base case
+    {
+        return;
+    }
+    // swap characters at the beginning and end of string
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    // run function again recursively to swap 1 character after the start and 1 before the end of the last call
+    passByValueReversal(str, start + 1, end - 1);
+}
+
+// Pass-by-Reference String Reversal Implementation
+void passByRefReversal(char *str) {
+    if(*str){ // checks to see if pointer points to a non empty string i.e not null
+        passByRefReversal(str+1); // recursive call to the function with the address of the second character in string
+        printf("%c", *str); // print character
+    }
+}
+
+// Palindrome implementation
+int isPalindrome(char *str, int left, int right){
+    if(str == NULL || left < 0 || right < 0 ){  // If the input is invalid or the substring is empty, it is not a palindrome
+        return 0;
+    }
+    if(left >= right){  // If the substring has only one character or no characters, it is a palindrome
+        return 1;
+    }
+    if(str[left] == str[right]){ // If the first and last characters of the substring are equal, check if the substring minus the first and last characters is also a palindrome
+        return isPalindrome(str, left+1, right-1);
+    }
+    return 0;
+}
+
 // mainline logic
 int main()
 {
@@ -145,13 +196,19 @@ int main()
     int runProgram = 1; // 0 is false 1 is true
 	while(runProgram == 1)
 	{
-        int m = menu();
+        int m = menu(); //display menu with options
+
+        // variables and data structures used by mainline logic
         int array_length = 0;
         int input_array[100];
         int bubble_arr[100];
         int sel_arr[100];
         int quick_arr[100];
-        switch(m){
+        char inputString[100]; // for option 2
+        char refString[100];
+        char palinString[100];
+
+        switch(m){ // program logic unfolds based on which option user chooses
             case 1:
                 // input capture and validation
                 printf("    Please enter the size of the array: ");
@@ -162,7 +219,7 @@ int main()
                     scanf("%d", &input_array[i]);
                 }
 
-                // copy values from input_array into 3 different arrays
+                // copy values from input_array into 3 different arrays for each sort
                 for(int i = 0; i < array_length; i++)
                 {
                     bubble_arr[i] = input_array[i];
@@ -172,85 +229,51 @@ int main()
 
                 // execute Bubble Sort:
                 printf("[1] Executing Bubble Sort to order the following array in descending order:\n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                    if(i == array_length-1)
-                    {
-                        printf("%d\n", input_array[i]);
-                    }
-                    else{
-                        printf("%d, ", input_array[i]);
-                    }
-                }
+                printArr(input_array, array_length);
                 bubbleSort(bubble_arr, array_length);
-                printf("    Printing the sorted array: \n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                   if(i == array_length-1)
-                   {
-                     printf("%d\n", bubble_arr[i]);
-                   }
-                   else{
-                     printf("%d, ", bubble_arr[i]);
-                   }
-                }
+                printf("    Bubble Sorted array: \n    ");
+                printArr(bubble_arr, array_length);
                 printf("--> Array sorted in: [%d moves]\n", bubble_moves);
 
                 // execute Selection Sort:
                 printf("[2] Executing Selection Sort to order the following array in descending order:\n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                    if(i == array_length-1)
-                    {
-                        printf("%d\n", sel_arr[i]);
-                    }
-                    else{
-                        printf("%d, ", sel_arr[i]);
-                    }
-                }
+                printArr(sel_arr, array_length);
                 selectionSort(sel_arr, array_length);
-                printf("    Printing the sorted array: \n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                    if(i == array_length-1)
-                    {
-                        printf("%d\n", sel_arr[i]);
-                    }
-                    else{
-                        printf("%d, ", sel_arr[i]);
-                    }
-                }
+                printf("    Selection Sorted array: \n    ");
+                printArr(sel_arr, array_length);
                 printf("--> Array sorted in: [%d moves]\n", selection_moves);
 
                 // execute Quick Sort:
                 printf("[3] Executing Quick Sort to order the following array in descending order:\n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                    if(i == array_length-1)
-                    {
-                        printf("%d\n", quick_arr[i]);
-                    }
-                    else{
-                        printf("%d, ", quick_arr[i]);
-                    }
-                }
+                printArr(quick_arr, array_length);
                 quickSort(quick_arr, 0, array_length-1);
-                printf("    Printing the sorted array: \n    ");
-                for(int i = 0; i < array_length; i++)
-                {
-                    if(i == array_length-1)
-                    {
-                        printf("%d\n", quick_arr[i]);
-                    }
-                    else{
-                        printf("%d, ", quick_arr[i]);
-                    }
-                }
+                printf("    Quick Sorted array: \n    ");
+                printArr(quick_arr, array_length);
                 printf("--> Array sorted in: [%d moves]\n", quick_moves);
 
                 break;
             case 2:
-                printf("Please enter a string of words to reverse: ");
+                getchar();
+                printf("[1] Please enter a string of words to reverse: ");
+                fgets(inputString, sizeof(inputString), stdin);
+                // copy input string into refString to be used for pass by reference function
+                for(int i = 0; i < sizeof(inputString); i++){
+                    refString[i] = inputString[i];
+                    palinString[i] = inputString[i];
+                }
+                printf("[2] Reversing string using Pass-by-Value... ");
+                passByValueReversal(inputString, 0, strlen(inputString)-1);
+                printf("%s\n", inputString);
+                printf("[3]Reversing string using Pass-by-Reference...");
+                passByRefReversal(refString);
+                //check isPalindrome result for palindrome and print statement accordingly
+                if(isPalindrome(palinString, 0, strlen(palinString)-2) == 1){
+                    printf("\n--> This string is a palindrome.");
+                }
+                else{
+                    printf("\n--> This string is not a palindrome.");
+                }
+                printf("\n");
                 break;
             default:
                 printf("You're not supposed to be here...");
@@ -267,7 +290,8 @@ int main()
             bubble_moves=0;
             selection_moves=0;
             quick_moves=0;
-            // empties all arrays for next round
+
+            // empties all arrays for next round of tests
             for(int i = 0; i < array_length; i ++){
                 input_array[i] = 0;
                 bubble_arr[i] = 0;
