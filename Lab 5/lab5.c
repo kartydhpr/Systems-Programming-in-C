7785bed6d5b342c29b29cee4d7532a27ecf172ec
Karteikay Dhuper <karteikaydhuper@Kartys-Laptop.home>
2023-04-08 21:09:47 -0400

    complete lab5

--- Lab 5/lab5.c
+++ Lab 5/lab5.c
@@ -11,6 +11,11 @@
 
 //global variables
 
+int stackLLCounter = 0;
+int stackArrCounter = 0;
+int queueLLCounter = 0;
+int queueArrCounter = 0;
+
 struct Student{
     char firstName[50];
     char lastName[50];
@@ -43,7 +48,7 @@ void printStudent(struct Student *student) {
     printf("GPA: %.0f\n", student->age);
 }
 
-// ------- STACK CODE -------
+// ------- STACK CODE FOR LINKED LIST AND ARRAY-------
 
 // insert a node at the end of the linked list
 void pushLL(struct Student **head, char *firstName, char *lastName, char *PUID, float *age){
@@ -88,7 +93,7 @@ void popLL(struct Student **head) {
     printStudent(last);
     free(last); // free the memory allocated for the last node
     temp->next = NULL; // set the next pointer of second last node to NULL
-    printf("Student popped.\n");
+    printf("Student popped from Linked List stack...\n");
 }
 
 // returns the size of the stack in terms of the number of node
@@ -102,17 +107,18 @@ int sizeLL(struct Student *head){
     return count;
 }
 
-// Returns a boolean, true if the stack has no nodes and false if the stack has greater than 0 node
-int emptyLL(struct Student *head){
-    if (head == NULL) {
-        return 1; // linked list is empty
+// Empties the linked list stack
+void emptyLL(struct Student **head) {
+    struct Student *current = *head;
+    while (current != NULL) {
+        struct Student *temp = current;
+        current = current->next;
+        free(temp);
     }
-    else {
-        return 0; // linked list is not empty
-    }
+    *head = NULL;
 }
 
-// insert a student node at the top of the array stack
+// insert a student node at the top of the array stack - function completed with the help of GPT-3
 void pushA(char *arrayStack, int *arrayTop, char *firstName, char *lastName, char *PUID, float *age) {
     if (*arrayTop == 99) { // array is full
         printf("Array stack is full. Cannot push more elements.\n");
@@ -135,7 +141,7 @@ void pushA(char *arrayStack, int *arrayTop, char *firstName, char *lastName, cha
         free(first_name);
         free(last_name);
 
-        printf("Pushed Student: %s %s %s %f\n", firstName, lastName, PUID, *age);
+        // printf("Pushed Student: %s %s %s %f\n", firstName, lastName, PUID, *age);
     }
 }
 
@@ -153,12 +159,13 @@ void popA(char *arrayStack, int *arrayTop) {
         memcpy(first_name, &arrayStack[index], 20);
         memcpy(last_name, &arrayStack[index + 20], 20);
 
-        printf("Popped Student: %s %s %s %f\n", first_name, last_name, &arrayStack[index + 40], *(float*)&arrayStack[index + 44]);
+        // printf("Popped Student: %s %s %s %f\n", first_name, last_name, &arrayStack[index + 40], *(float*)&arrayStack[index + 44]);
 
         free(first_name);
         free(last_name);
 
         (*arrayTop)--; // decrement top of the stack
+        printf("Student popped from Array stack...\n");
     }
 }
 
@@ -167,14 +174,17 @@ int sizeA(int arrayTop){
     return arrayTop + 1;
 }
 
-// Returns a boolean, true if the array has no students and false if it has greater than 0 students
-int emptyA(int arrayTop){
-    return (arrayTop == -1);
+// Empties the array stack
+void emptyA(struct Student *arr[], int *top) {
+    *top = -1; // reset top index to -1
+    for (int i = 0; i < 100; i++) { // 100 is an arbitrary max stack size ive assigned
+        arr[i] = NULL; // set all array elements to NULL
+    }
 }
 
-// ------- QUEUE CODE -------
+// ------- QUEUE CODE FOR LINKED LIST AND ARRAY -------
 
-// insert a node at the end of the linked list
+// insert a node at the end of the linked list to mimic a queue
 void enqueueLL(struct Student **head, char *firstName, char *lastName, char *PUID, float *age){
     struct Student *new_student = (struct student*) malloc(sizeof(struct Student));
     strncpy(new_student->firstName, firstName, 50);
@@ -195,6 +205,36 @@ void enqueueLL(struct Student **head, char *firstName, char *lastName, char *PUI
     current_student->next = new_student; // set next pointer of last node to point to new node
 }
 
+// insert a student node at the back of the array queue
+void enqueueA(char *arrayQueue, int *queueFront, int *queueBack, char *firstName, char *lastName, char *PUID, float *age) {
+    if (*queueBack == 99) { // queue is full
+        printf("Array queue is full. Cannot enqueue more elements.\n");
+    } else {
+        (*queueBack)++; // increment back of the queue
+        int index = *queueBack * 5; // calculate index of where to add the student node
+
+        // dynamically allocate memory for the student names
+        char *first_name = (char*) malloc(sizeof(char) * 20);
+        char *last_name = (char*) malloc(sizeof(char) * 20);
+        strncpy(first_name, firstName, 20);
+        strncpy(last_name, lastName, 20);
+
+        // copy student data to the array queue
+        memcpy(&arrayQueue[index], first_name, 20);
+        memcpy(&arrayQueue[index + 20], last_name, 20);
+        memcpy(&arrayQueue[index + 40], PUID, 4);
+        memcpy(&arrayQueue[index + 44], age, sizeof(float));
+
+        free(first_name);
+        free(last_name);
+
+        // printf("Enqueued Student: %s %s %s %.0f\n", firstName, lastName, PUID, *age);
+        if (*queueFront == -1) { // if the queue was previously empty
+            *queueFront = 0; // set the front to the first element
+        }
+    }
+}
+
 // delete the first node in the list
 void dequeueLL(struct Student **head){
     // check if list is empty
@@ -202,21 +242,38 @@ void dequeueLL(struct Student **head){
         printf("There are no students to delete....\n");
         return;
     }
+    printf("Here is the student you are dequeue-ing: \n");
+
     // store the first node in a temporary variable
     struct Student *temp = *head;
+    printStudent(temp);
     // move the head to point to the second node
     *head = (*head)->next;
     // free the memory allocated to the first node
     free(temp);
-    printf("Deleted the first student in the list.\n");
+    printf("Dequeued the first student in the linked list.\n");
+}
+
+// remove and return the first student node from the array stack - function completed with the help of GPT-3
+void dequeueA(char arrayQueue[], int* front, int* back) {
+    if (*front == -1) { // check if queue is empty
+        printf("Queue is empty\n");
+    } else {
+        printf("Dequeued element from array: %c\n", arrayQueue[*front]); // print dequeued element
+        if (*front == *back) { // reset front and back if there is only one element
+            *front = -1;
+            *back = -1;
+        } else {
+            *front = (*front + 1) % sizeof(arrayQueue); // move front to the next element in the circular array
+        }
+    }
 }
 
 // print function formatted to look like a horizontal queue
 void printQueue(struct Student *head) {
     struct Student *current = head;
-    printf("Traversing the list:\n");
     while (current != NULL) {
-        printf("(Name: %s %s, PUID: %s, Age: %.0f) --> ", current->firstName, current->lastName, current->PUID, current->age);
+        printf("(Name: %s %s, PUID: %s, Age: %.0f) <-- ", current->firstName, current->lastName, current->PUID, current->age);
         current = current->next; // go the next node if current is not null
     }
     printf("NULL\n");
@@ -235,22 +292,25 @@ void printStack(struct Student *head) {
 // Navigation Menu
 int menu()
 {
-    int menu_decision = 10;
-    while(menu_decision > 9 || menu_decision < 1) { // makes sure program doesn't crash
+    int menu_decision = 12;
+    while(menu_decision > 11 || menu_decision < 1) { // makes sure program doesn't crash
         printf("[!] ------------------------------------ Menu ---------------------------------- [!] \n");
-        printf(" | -------- LL Stack Operations -------- \n");
-        printf(" | • Option 1: Push to LL Stack \n");
-        printf(" | • Option 2: Pop from LL Stack\n");
-        printf(" | • Option 3: Size of Linked List \n");
-        printf(" | • Option 4: Empty Linked List \n");
-        printf(" | ------- Array Stack Operations ------- \n");
-        printf(" | • Option 5: Push to Array Stack \n");
-        printf(" | • Option 6: Pop from Array Stack\n");
-        printf(" | • Option 7: Size of Array List \n");
-        printf(" | • Option 8: Empty Array List \n");
-        printf(" | • Option 9: Print Stack \n");
+        printf(" | -------- Stack Operations -------- \n");
+        printf(" | • Option 1: Push to Stack \n");
+        printf(" | • Option 2: Pop from Stack \n");
+        printf(" | • Option 3: Empty Stack \n");
+        printf(" | • Option 4: Print Stack \n");
+        printf(" | ------- Queue Operations ------- \n");
+        printf(" | • Option 5: Enqueue Queue \n");
+        printf(" | • Option 6: Dequeue Queue\n");
+        printf(" | • Option 7: Empty Queue \n");
+        printf(" | • Option 8: Print Queue \n");
+        printf(" | --- Stack + Queue Operations --- \n");
+        printf(" | • Option 9: Add Element (Push and Enqueue) \n");
+        printf(" | • Option 10: Remove Element (Pop and Dequeue) \n");
+        printf(" | • Option 11: Number of moves so far \n");
         printf("[!] ---------------------------------------------------------------------------- [!] \n");
-        printf("Please return the number of the option you want to run (1-9): ");
+        printf("Please return the number of the option you want to run (1-11): ");
         scanf("%d", &menu_decision);
     }
     return(menu_decision);
@@ -260,11 +320,20 @@ int menu()
 int main()
 {
     printf("Program Starting...\n");
-    // struct head declared and list created before while loop to keep list persistent with each rerun of logic
+    // struct heads declared and lists created before while loop to keep list persistent with each rerun of while logic
     struct Student *head;
-    createListNoNodes(&head); // create an empty linked list
+    struct Student *queueHead;
+    createListNoNodes(&head); // create an empty linked list for the stack
+    createListNoNodes(&queueHead); // create an empty linked list for the queue
+
     char arrayStack[100]; // // init array for array stack
     int arrayTop = -1; // to keep track of
+
+    char arrayQueue[100];
+    int queueFront = -1; // initialize the front and back of the queue
+    int queueBack = -1;
+    int sizeQArr = sizeof(arrayQueue) / sizeof(arrayQueue[0]);
+
 
     int runProgram = 1; // 0 is false 1 is true
 	while(runProgram == 1)
@@ -279,7 +348,7 @@ int main()
 
         switch(m){ // program logic unfolds based on which option user chooses
             case 1:
-                printf("Choice confirmed: Push to LL Stack \n");
+                printf("Choice confirmed: Pushing to Linked List and Array Stack \n");
                 printf("Enter first name: ");
                 scanf("%s", &scanFName);
                 printf("Enter last name: ");
@@ -289,29 +358,35 @@ int main()
                 printf("Enter age: ");
                 scanf("%f", &scanAge);
                 pushLL(&head, scanFName, scanLName, scanPUID, &scanAge);
-                printf("Student pushed to stack\n");
+                stackLLCounter ++;
+                printf("Student pushed to Linked List Stack...\n");
+                pushA(arrayStack, &arrayTop, scanFName, scanLName, scanPUID, &scanAge);
+                stackArrCounter ++;
+                printf("Student pushed to Array Stack...\n");
                 break;
             case 2:
-                printf("Choice confirmed: Pop from LL Stack \n");
+                printf("Choice confirmed: Popping from Linked list and Array Stack \n");
                 popLL(&head);
+                stackLLCounter ++;
+                popA(arrayStack, &arrayTop);
+                stackArrCounter ++;
                 break;
             case 3:
-                printf("Choice confirmed: Size of Linked List \n");
-                int size = sizeLL(head);
-                printf("Size of linked list: %d\n", size);
+                printf("Choice confirmed: Emptying Linked List and Array stacks \n");
+                emptyLL(&head);
+                printf("Linked List stack emptied...\n");
+                emptyA(arrayStack, &arrayTop);
+                printf("Array stack emptied...\n");
                 break;
             case 4:
-                printf("Choice confirmed: Empty Linked List \n");
-                int listEmpty = emptyLL(head);
-                if (listEmpty == 1){
-                    printf("True, the linked list is empty! \n");
-                }
-                else if(listEmpty == 0){
-                    printf("False, the linked list is not empty. \n");
-                }
+                printf("Choice confirmed: Printing Stack \n");
+                int size = sizeLL(head);
+                printStack(head);
+                printf("Size of linked list stack: %d\n", size);
+                printf("Size of array stack: %d\n", sizeA(arrayTop));
                 break;
             case 5:
-                printf("Choice confirmed: Push to Array Stack \n");
+                printf("Choice confirmed: En-queueing Linked List and Array queues \n");
                 printf("Enter first name: ");
                 scanf("%s", &scanFName);
                 printf("Enter last name: ");
@@ -320,27 +395,73 @@ int main()
                 scanf("%s", &scanPUID);
                 printf("Enter age: ");
                 scanf("%f", &scanAge);
-                pushA(arrayStack, &arrayTop, scanFName, scanLName, scanPUID, &scanAge);
+                enqueueLL(&queueHead, scanFName, scanLName, scanPUID, &scanAge);
+                queueLLCounter ++;
+                printf("Student enqueued to Linked List queue...\n");
+                enqueueA(arrayQueue, &queueFront, &queueBack, scanFName, scanLName, scanPUID, &scanAge);
+                queueArrCounter ++;
+                printf("Student enqueued to Array queue...\n");
                 break;
             case 6:
-                printf("Choice confirmed: Pop from Array Stack \n");
-                popA(arrayStack, &arrayTop);
+                printf("Choice confirmed: De-queueing Linked List and Array queues \n");
+                dequeueLL(&queueHead);
+                queueLLCounter ++;
+                printf("Student dequeued from Linked List queue...\n");
+                dequeueA(arrayQueue, &queueFront, &queueBack);
+                queueArrCounter ++;
+                printf("Student dequeued from Array queue...\n");
                 break;
             case 7:
-                printf("Choice confirmed: Size of Array \n");
-                printf("Size of array stack: %d\n", sizeA(arrayTop));
+                printf("Choice confirmed: Emptying Linked List and Array queues\n");
+                emptyLL(&queueHead);
+                printf("Linked List queue emptied...\n");
+                emptyA(arrayQueue, &arrayTop);
+                printf("Array stack emptied...\n");
                 break;
             case 8:
-                printf("Choice confirmed: Empty Array \n");
-                if (emptyA(arrayTop)) {
-                    printf("True, the array stack is empty! \n");
-                } else {
-                    printf("False, the array stack is not empty. \n");
-                }
+                printf("Choice confirmed: Printing Queue\n");
+                printQueue(queueHead);
                 break;
             case 9:
-                printf("Choice confirmed: Print Stack\n");
-                printStack(head);
+                printf("Choice confirmed: Adding elements to Linked List and Array stacks and queues\n");
+                printf("Enter first name: ");
+                scanf("%s", &scanFName);
+                printf("Enter last name: ");
+                scanf("%s", &scanLName);
+                printf("Enter PUID : ");
+                scanf("%s", &scanPUID);
+                printf("Enter age: ");
+                scanf("%f", &scanAge);
+                pushLL(&head, scanFName, scanLName, scanPUID, &scanAge);
+                stackLLCounter ++;
+                printf("Pushed to linked list stack\n");
+                pushA(arrayStack, &arrayTop, scanFName, scanLName, scanPUID, &scanAge);
+                stackArrCounter ++;
+                printf("Pushed to array stack\n");
+                enqueueLL(&queueHead, scanFName, scanLName, scanPUID, &scanAge);
+                queueLLCounter ++;
+                printf("Enqueued to linked list queue\n");
+                enqueueA(arrayQueue, &queueFront, &queueBack, scanFName, scanLName, scanPUID, &scanAge);
+                queueArrCounter ++;
+                printf("Enqueued to array queue...\n");
+                break;
+            case 10:
+                printf("Choice confirmed: Removing elements from Linked List and Array stacks and queues\n");
+                popLL(&head);
+                stackLLCounter ++;
+                popA(arrayStack, &arrayTop);
+                stackArrCounter ++;
+                dequeueLL(&queueHead);
+                queueLLCounter ++;
+                dequeueA(arrayQueue, &queueFront, &queueBack);
+                queueArrCounter ++;
+                break;
+            case 11:
+                printf("Choice confirmed: Returning number of moves so far for Linked List and Array\n");
+                printf("Number of moves made on Linked List stack: %d\n", stackLLCounter);
+                printf("Number of moves made on Array stack: %d\n", stackLLCounter);
+                printf("Number of moves made on Linked List queue: %d\n", queueLLCounter);
+                printf("Number of moves made on Array queue: %d\n", queueArrCounter);
                 break;
         }
 		printf("Do you want to perform more operations? (y/n): ");
